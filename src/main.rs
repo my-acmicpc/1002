5aa1e178cbb1bb486d07a9ea6f2c8023b59cd016
Juyeong Maing <mjy9088@naver.com>
2024-03-04 22:58:50 +0900

    remove unnecessary lambda

--- src/main.rs
+++ src/main.rs
@@ -49,7 +49,7 @@ fn main() {
     for _ in 0..test_case_count {
         let mut line = String::new();
         io::stdin().read_line(&mut line).unwrap();
-        let mut iter = line.trim().split(' ').flat_map(|x| x.parse::<i64>());
+        let mut iter = line.trim().split(' ').flat_map(&str::parse::<i64>);
         let result = solution(
             iter.next().unwrap(),
             iter.next().unwrap(),
